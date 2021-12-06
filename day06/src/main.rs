345dc7c3322c62ce3be98f345db25bd427ebc218
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2021-12-06 23:16:55 +0100

    Day 06.

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,35 +1,35 @@
 const SAMPLE: &str = include_str!("sample.txt");
 const INPUT: &str = include_str!("input.txt");
 
-fn parse(input: &str) -> Vec<u32> {
+fn parse(input: &str) -> Vec<u64> {
   input.split(',').flat_map(|n| n.parse()).collect()
 }
 
-fn solve1(input: &str) -> u32 {
-  let mut counters = parse(input);
+fn solve(input: &str, days: usize) -> u64 {
+  let counters = parse(input);
+  let mut ages = [0; 9];
 
-  for _ in 0..80 {
-    let mut newborn = 0;
-
-    for counter in &mut counters {
-      if *counter == 0 {
-        *counter = 6;
-        newborn += 1;
-      } else {
-        *counter -= 1;
-      }
-    }
-
-    for _ in 0..newborn {
-      counters.push(8);
-    }
+  for counter in counters {
+    ages[counter as usize] += 1;
   }
 
-  counters.len() as _
+  for _ in 0..days {
+    for i in 0..8 {
+      ages.swap(i, i + 1);
+    }
+
+    ages[6] += ages[8];
+  }
+
+  ages.into_iter().sum()
 }
 
 fn main() {
   println!("part1");
-  println!("sample: {}", solve1(SAMPLE));
-  println!("input: {}", solve1(INPUT));
+  println!("sample: {}", solve(SAMPLE, 80));
+  println!("input: {}", solve(INPUT, 80));
+
+  println!("part2");
+  println!("sample: {}", solve(SAMPLE, 256));
+  println!("input: {}", solve(INPUT, 256));
 }
